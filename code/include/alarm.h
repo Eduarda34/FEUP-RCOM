06dd9ad9e7316d69e0fcd64b23980e25f7bb60bf
Clarisse Carvalho <clarissemtcarvalho@gmail.com>
2024-10-02 10:12:10 +0100

    add functions talked in class

---
+++ code/include/alarm.h
@@ -0,0 +1,24 @@
+// Alarm functions header.
+
+#ifndef _ALARM_H_
+#define _ALARM_H_
+
+
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+
+// Main Alarm Functions 
+void reset_alarm_count();
+void alarm_handler(int signal);
+
+// Getters
+int get_alarm_count();
+int get_alarm_flag();
+
+// Setters
+void set_alarm_flag(int flag);
+
+
+
+#endif // _ALARM_H_
