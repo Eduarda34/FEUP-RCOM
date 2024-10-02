0a3b9d17c4ceba2213ef9eb5b860dc5f0a52a052
Clarisse Carvalho <clarissemtcarvalho@gmail.com>
2024-10-02 10:13:44 +0100

    init functions alarm

---
+++ code/src/alarm.c
@@ -0,0 +1,24 @@
+// Alarm functions implementation
+
+int alarm_flag = FALSE;
+int alarm_count = 0;
+
+void reset_alarm_count(){
+
+}
+
+void alarm_handler(int signal) {
+
+}
+
+int get_alarm_count() {
+    return 0;
+}
+
+int get_alarm_flag() {
+    return 0;
+}
+
+void set_alarm_flag(int flag) {
+    
+}
