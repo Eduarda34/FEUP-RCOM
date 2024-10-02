4b5d10284f14ac8b5616ff02e87798eefefc05c2
Clarisse Carvalho <clarissemtcarvalho@gmail.com>
2024-10-02 10:15:45 +0100

    update sets and getter in alarm functions

--- code/src/alarm.c
+++ code/src/alarm.c
@@ -4,21 +4,24 @@ int alarm_flag = FALSE;
 int alarm_count = 0;
 
 void reset_alarm_count(){
-
+    alarm_count = 0;
 }
 
 void alarm_handler(int signal) {
+    set_alarm_flag(TRUE);
+    alarm_count++;
 
+    printf("Alarm count: #%d\n", alarm_count);
 }
 
 int get_alarm_count() {
-    return 0;
+    return alarm_count;
 }
 
 int get_alarm_flag() {
-    return 0;
+    return alarm_flag;
 }
 
 void set_alarm_flag(int flag) {
-    
+    alarm_flag = flag;
 }
