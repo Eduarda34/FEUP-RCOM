f151dc5c678555e6e5eb525caaef29dc3d2e645b
eduardaraujo <mpmaeduarda2002@gmail.com>
2024-10-07 17:25:05 +0100

    some changes in link_layer

--- code/src/link_layer.c
+++ code/src/link_layer.c
@@ -2,6 +2,10 @@
 
 #include "link_layer.h"
 #include "serial_port.h"
+#include "state.h"
+
+#include <iostream>
+#include <string.h>
 
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
@@ -11,38 +15,38 @@ unsigned char byte;
 int timeout = 0;
 int nretransmissions = 0;
 
-int llopen(LinkLayer connectionParameters)
-{
-    int fd = openSerialPort(connectionParameters.serialPort,
-                       connectionParameters.baudRate);
+
+
+int llopen(LinkLayer connectionParameters) {
+    int fd = openSerialPort(connectionParameters.serialPort, connectionParameters.baudRate);
 
     // Check if openSerialPort has error
-    if (fd < 0)
-    {
-        return -1;
+    if (fd < 0) {
+        printf("%s", connectionParameters.serialPort);
+        perror(connectionParameters.serialPort);
+        exit(-1);
     }
+
+    
 
     // TODO
 
     return 1;
 }
 
-int llwrite(const unsigned char *buf, int bufSize)
-{
+int llwrite(const unsigned char *buf, int bufSize) {
     // TODO
 
     return 0;
 }
 
-int llread(unsigned char *packet)
-{
+int llread(unsigned char *packet) {
     // TODO
 
     return 0;
 }
 
-int llclose(int showStatistics)
-{
+int llclose(int showStatistics) {
     // TODO
 
     int clstat = closeSerialPort();
