0a0b8674d1b2aa729960b06e6d18608d354aaa71
eduardaraujo <mpmaeduarda2002@gmail.com>
2024-10-16 09:07:32 +0100

    documentation deleted on frame.c

--- code/src/frame.c
+++ code/src/frame.c
@@ -1,13 +1,5 @@
 #include "frame.h"
 
-
-/**
- * @brief initializes an S-frame with specified address and control information
- * 
- * @param frame pointer to the S-frame structure to be initialized
- * @param address address byte to be set in the frame
- * @param control control byte to be set in the frame
- */
 void init_s_frame(s_frame *frame, uint8_t address, uint8_t control) {
     frame->flag_start = FLAG;
     frame->address = address;
@@ -16,14 +8,7 @@ void init_s_frame(s_frame *frame, uint8_t address, uint8_t control) {
     frame->flag_end = FLAG;
 }
 
-/**
- * @brief initializes an I-frame with specified data, length, and packet number
- * 
- * @param frame pointer to the I-frame structure to be initialized
- * @param data pointer to the data to be included in the frame
- * @param data_len length of the data to be copied into the frame
- * @param packet packet number, which is shifted and stored in the control field
- */
+
 void init_i_frame(i_frame *frame, const uint8_t *data, int data_len, int packet) {
     frame->flag_start = FLAG;
     frame->address = ADDR_E;
@@ -48,10 +33,7 @@ void init_i_frame(i_frame *frame, const uint8_t *data, int data_len, int packet)
     frame->buffer = (uint8_t *)malloc(frame->buffer_len);
 }
 
-/**
- * @brief fills the buffer of an I-frame with its components
- * @param frame pointer to the I-frame structure whose buffer will be filled
- */
+
 void fill_i_frame_buffer(i_frame *frame) {
     frame->buffer[0] = frame->flag_start;
     frame->buffer[1] = frame->address;
@@ -68,10 +50,6 @@ void fill_i_frame_buffer(i_frame *frame) {
     frame->buffer[frame->data_len + 5] = frame->flag_end;
 }
 
-/**
- * @brief frees the allocated memory for an I-frame
- * @param frame pointer to the I-frame structure whose resources will be freed
- */
 void free_i_frame(i_frame *frame) {
     free(frame->data);
     free(frame->buffer);
