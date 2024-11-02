d629da8a21d82acc2a302f2158531a0866b32179
Clarisse Carvalho <clarissemtcarvalho@gmail.com>
2024-11-02 08:32:29 +0000

    create i frame now is a *

--- code/src/link_layer.c
+++ code/src/link_layer.c
@@ -1,67 +1,199 @@
 // Link layer protocol implementation
 #include "link_layer.h"
 
-
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
 
-// Global Variables
-unsigned char byte;
-int timeout = 0;
-int nretransmissions = 0;
+struct termios oldtioT;
 int fd;
-struct termios oldtio; 
 static uint8_t sequence_number = 0;
 
 
+//////////////////////////////////////////////// FALTA CORRIGIR READ AND SEND MESSAGE
+//////////////////////////////////////////////// FALTA CORRIGIR llopen MAS VOU DORMIR
 
 
-int llopen(LinkLayer connectionParameters) {
-    struct termios newtio;
+////////////////////////////////////////////////
+/// AUX                                      ///
+////////////////////////////////////////////////
+int read_message(int fd, uint8_t *buf, int buf_size, command response){
+    int i = 0;
+    reset_state();
+    set_command(response);
+    while (get_curr_state() != FINAL_STATE && !get_alarm_flag()) {
+        if (i >= buf_size) break;
+        int bytes = read(fd, buf + i, 1);
+        if (bytes != -1) update_state(buf[i]);
+        i++;
+    }
+    if (get_curr_state() != FINAL_STATE) {
+        printf("Failed to get response!\n");
+        return -1;
+    }
+    return i;
+}
 
-    // Get current port settings for backup
-    if (tcgetattr(fd, &oldtio) == -1) {
-        perror("tcgetattr\n");
+int send_message(int fd, uint8_t *frame, int msg_size, command response)
+{
+    int bytes;
+    if (response == NO_RESP) { 
+        if ((bytes = write(fd, frame, msg_size)) == -1) {
+            printf("Write failed\n");
+            return -1;
+        }
+        return bytes;
+    }
+    set_command(response);
+    reset_alarm_count();
+    reset_state();
+    while (get_alarm_count() < 3 && get_curr_state() != FINAL_STATE) {
+        set_alarm_flag(FALSE);
+        if ((bytes = write(fd, frame, msg_size)) == -1) {
+            printf("Write failed\n");
+            return -1;
+        }
+        printf("Message sent\n");
+        unsigned char buf[MSG_MAX_SIZE*2] = {0};
+        alarm(3);
+        int i = 0;
+        while (get_curr_state() != FINAL_STATE && !get_alarm_flag()) {
+            if (i >= (MSG_MAX_SIZE*2)) {
+                continue;
+            }
+            int read_byte = read(fd, buf + i, 1);
+            if (read_byte != -1) {
+                update_state(buf[i]);
+            }
+            i++;
+        }
+    }
+
+    if (get_curr_state() != FINAL_STATE) {
+        printf("Failed to get response!\n");
         return -1;
     }
 
-    memset(&newtio, 0, sizeof(newtio));  // Clear the newtio structure
+    return bytes;
+}
 
-    printf("********** New structure **********\n");
+uint8_t *create_s_frame_buffer(uint8_t address, uint8_t control) {
+    uint8_t *buffer = (uint8_t *)malloc(5);
+    if (buffer == NULL) {
+        return NULL; // Handle memory allocation failure
+    }
+    memset(buffer, 0, 5);
+    buffer[0] = FLAG;
+    buffer[1] = address;
+    buffer[2] = control;
+    buffer[3] = BCC(buffer[1], buffer[2]);
+    buffer[4] = FLAG;
+    return buffer;
+}
 
-    //(void)signal(SIGALRM, alarm_handler);
+int send_s_frame(int fd, uint8_t address, uint8_t control, command response) {
+    uint8_t *buffer = create_s_frame_buffer(address, control);
+    if (buffer == NULL) {
+        return -1; 
+    }
+    int bytes = send_message(fd, buffer, 5, response);
+    free(buffer);
+    return bytes;
+}
 
-    switch (connectionParameters.role) {
-        case LlTx:
-            printf("Start Transmissor\n");
-            set_role(TRANSMITTER);
-            //(void)signal(SIGALRM, alarm_handler);
-            if (startTransmissor(fd) < 0) {
-                perror("Error starting transmitter\n");
-                return -1;
-            }
-            
-            break;
 
-        case LlRx:
-            set_role(RECEIVER);
-            //(void)signal(SIGALRM, alarm_handler);
-            if (startReceiver(fd) < 0) {
-                perror("Error starting transmitter\n");
-                return -1;
-            }
-            break;
+////////////////////////////////////////////////
+/// LLOPEN                                   ///
+////////////////////////////////////////////////
+int start_transmissor(int fd){
+    return send_s_frame(fd, ADDR, 0x03, RESPONSE_UA);
+}
 
-        default:
-            // Handle unknown roles
-            fprintf(stderr, "Unknown role: %d\n", connectionParameters.role);
-            return -1;
+int start_receiver(int fd){
+    unsigned char message[5];
+    if (read_message(fd, message, 5, COMMAND_SET) < 0) return -1;
+    return send_s_frame(fd, ADDR, 0x07, NO_RESP);
+}
 
+int llopen(LinkLayer connectionParameters) {
+
+    // Open serial port device for reading and writing and not as controlling tty
+    // because we don't want to get killed if linenoise sends CTRL-C.
+    fd = open(connectionParameters.serialPort, O_RDWR | O_NOCTTY);
+
+    if (fd < 0)     {
+        printf("%s", connectionParameters.serialPort);
+        perror(connectionParameters.serialPort);
+        exit(-1);
     }
 
-    printf("********** Connection Successfully **********\n");
+    struct termios newtio;
+
+    // Save current port settings
+    if (tcgetattr(fd, &oldtioT) == -1)  {
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    // Clear struct for new port settings
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = connectionParameters.baudRate | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode (non-canonical, no echo,...)
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0;
+    switch (connectionParameters.role)
+    {
+    case TRANSMITTER:
+        newtio.c_cc[VMIN] = 0;                             
+        newtio.c_cc[VTIME] = connectionParameters.timeout; 
+        break;
+    case RECEIVER:
+        newtio.c_cc[VMIN] = 1; 
+        break;
+    }
+
+    tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1) {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("Start program. Please wait...\n");
+
+    switch (connectionParameters.role) {
+    case TRANSMITTER:
+        set_role(TRANSMITTER);
+        (void)signal(SIGALRM, alarm_handler);
+        if (start_transmissor(fd) < 0) {
+            printf("Could not start TRANSMITTER\n");
+            return -1;
+        }
+        break;
+
+    case RECEIVER:
+        set_role(RECEIVER);
+        (void)signal(SIGALRM, alarm_handler);
+
+        if (start_receiver(fd) < 0) {
+            printf("Could not start RECEIVER\n");
+            return -1;
+        }
+
+        break;
+    }
+
+    printf("-----------------------------------\n");
+    printf("Connection Successfully\n");
+    printf("-----------------------------------\n");
+
     return fd;
 }
+
 
 ////////////////////////////////////////////////
 // LLWRITE                                   ///
@@ -85,43 +217,52 @@ int stuff_message(uint8_t *buffer, int start, int msg_size, uint8_t *stuffed_msg
     return i;
 }
 
-void prepare_buffer(uint8_t *buffer, const uint8_t *data, int data_len, int packet) {
-    buffer[0] = FLAG;
-    buffer[1] = ADDR_E;
-    buffer[2] = (packet << 6);
-    buffer[3] = BCC(buffer[1], buffer[2]);
-
-    uint8_t bcc2 = 0;
-    for (int i = 0; i < data_len; i++) {
-        buffer[i + 4] = data[i];
-        bcc2 ^= data[i];
-    }
-    buffer[data_len + 4] = bcc2;
-}
-
 int create_stuffed_message(uint8_t *buffer, int msg_len, uint8_t *stuffed_msg) {
     msg_len = stuff_message(buffer, 4, msg_len, stuffed_msg);
     stuffed_msg[msg_len++] = FLAG; 
     return msg_len;
 }
 
-int transmit_frame(int fd, const uint8_t *data, int data_len, int packet){
-    int msg_len = data_len + 5;
+uint8_t *create_i_frame_buffer(const uint8_t *data, int data_len, int packet) {
+    int msg_len = data_len + 5; // Header (4 bytes) + BCC2 (1 byte)
     uint8_t *buffer = (uint8_t *)malloc(msg_len);
-    if (!buffer) {
+    if (buffer == NULL) {
         perror("Memory allocation failed");
-        return -1;
+        exit(EXIT_FAILURE);
     }
 
-    // PREPARE BUFFER
-    prepare_buffer(buffer, data, data_len, packet); 
+    // INIT HEADER
+    buffer[0] = FLAG;
+    buffer[1] = ADDR_E;
+    buffer[2] = (packet << 6); // Control field with sequence number
+    buffer[3] = BCC(buffer[1], buffer[2]);
+
+    // COMPUTE BCC2
+    uint8_t bcc2 = 0;
+    for (int i = 0; i < data_len; i++) {
+        buffer[i + 4] = data[i];
+        bcc2 ^= data[i]; 
+    }
+    buffer[data_len + 4] = bcc2;
+    return buffer;
+}
+
+int transmit_information_frame(int fd, const uint8_t *data, int data_len, int packet) {
+    // Call create_i_frame_buffer to allocate and prepare the I-frame
+    uint8_t *buffer = create_i_frame_buffer(data, data_len, packet);
+    if (!buffer) {
+        return -1; // Handle memory allocation failure
+    }
+
+    // Buffer length includes header, data, BCC2, and FLAG
+    int msg_len = data_len + 5;
     uint8_t stuffed_msg[msg_len * 2];
 
-    // CREATE STUFFED MESSAGE
+    // Create stuffed message
     msg_len = create_stuffed_message(buffer, msg_len, stuffed_msg);
+    free(buffer); // Free the allocated I-frame buffer after stuffing
 
-    free(buffer);
-
+    // Sending process
     for (int w = 0; w < 3; w++) {
         int bytes = send_message(fd, stuffed_msg, msg_len, RESPONSE_REJ);
         if (bytes == -1) {
@@ -129,7 +270,7 @@ int transmit_frame(int fd, const uint8_t *data, int data_len, int packet){
             return -1;
         }
 
-        // CHECK IF OKAY OR REJ
+        // Check response
         if ((packet == 0 && get_prev_response() == PA_F1) ||
             (packet == 1 && get_prev_response() == PA_F0)) {
             printf("Positive Acknowledgement :)\n");
@@ -147,7 +288,7 @@ int transmit_frame(int fd, const uint8_t *data, int data_len, int packet){
 
 int llwrite(const unsigned char *buf, int bufSize)
 {
-    int bytes = transmit_frame(fd, buf, bufSize, sequence_number);
+    int bytes = transmit_information_frame(fd, buf, bufSize, sequence_number);
     if (bytes == -1) {
         perror("llwrite: Error sending I-frame");
         exit(EXIT_FAILURE);
@@ -313,6 +454,7 @@ int llclose(int showStatistics) {
     }
 
     printf("Closing connection...\n");
+    printf("-----------------------------------\n");
     sleep(1); 
 
     if (tcsetattr(fd, TCSANOW, &oldtioT) == -1)
