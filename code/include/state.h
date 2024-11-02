0289d74666182722a793a2f35bc7bdfa14a9fb34
Clarisse Carvalho <clarissemtcarvalho@gmail.com>
2024-11-02 00:52:49 +0000

    add more comments to state.h and fix alarm mini error

--- code/include/state.h
+++ code/include/state.h
@@ -8,146 +8,73 @@
 #define ESCAPE 0x7d
 #define ADDR 0x03
 
-#define TRUE 1
-#define FALSE 0
-
-#define BIT(n) (1 << n)
-
-
-typedef enum {
-    COMMAND_SET,        // Set Command 
-    COMMAND_DISC,       // Disconnect Command
-    COMMAND_DATA,       // ??
-    R_UA,               // ??
-    R_RR_REJ,           // ?? 
+typedef enum
+{
+    COMMAND_SET,        // SET COMMAND 
+    COMMAND_DISC,       // DISCONNECT COMMAND 
+    COMMAND_DATA,       // DATA TRANSFER COMMAND
+    RESPONSE_UA,        // UNNUMBERED ACKNOWLEDGMENT RESPONSE
+    RESPONSE_REJ        // RECEIVER READY/REJECT RESPONSES
 } command;
 
-
-typedef enum {
-    TRANSMITTER,    // Machine Role Transmitter
-    RECEIVER        // Machine Role Receiver
+typedef enum
+{
+    TRANSMITTER,
+    RECEIVER
 } role;
 
-
 typedef enum {
-    R_NULL,         // ??
-    UA,             // ??
-    PR_0,           // ??
-    PR_1,           // ??
-    REJ_0,          // ??
-    REJ_1           // ??
+    RESPONSE_NULL,      // INIT STATE
+    UA,                 // UNNUMBERED ACKNOWLEDGMENT RECEIVED
+    PA_F0,              // Positive acknowledgment (RR) for frame 0
+    PA_F1,              // Positive acknowledgment (RR) for frame 1
+    REJ_F0,             // Reject (REJ) for frame 0
+    REJ_F1              // Reject (REJ) for frame 1
 } response;
 
-
 typedef enum {
-    START,          // Start State
-    FLAG_RCV,       // Flag State Received 
-    ADDRESS_RCV,    // Address Received
-    CONTROL_RCV,    // Control State Received 
-    DATA_RCV,       // Data State Received
-    BCC_VER,        // BCC Verification Passed State
-    STOP            // Stop State
+    START_STATE,        // Init State
+    FLAG_RCV,           // Flag Byte Received 
+    ADDRESS_RCV,        // Address Byte Received
+    CONTROL_RCV,        // Control Byte Received 
+    DATA_RCV,           // Data Byte Received
+    BCC_VER,            // BCC Verification Passed
+    FINAL_STATE         // Final State
 } state;
 
-
-
 typedef struct  {
-    state CURR_STATE;       // Current State
-    uint8_t ADDRESS;        // Address Byte
-    uint8_t CONTROL;        // Control Byte
-    response PREV_RESPONSE; // Previous response
-    role CURR_ROLE;         // Current Role
-    command CURR_COMMAND;   // Current Comand 
+    state curr_state;       // Current State
+    uint8_t address;        // Address Byte
+    uint8_t control;        // Control Byte
+    response prev_response; // Previous response
+    role curr_role;         // Current Role
+    command curr_command;   // Current Comand 
 } state_machine;
 
-
-
-// Getters
-/**
- * @brief Get the address object
- * @return unsigned char 
- */
-unsigned char get_address();
-
-/**
- * @brief Get the control object
- * @return unsigned char 
- */
-unsigned char get_control();
-
-/**
- * @brief Get the curr state object
- * @return state 
- */
+////////////////////////////////////////////////
+// GETTERS
+////////////////////////////////////////////////
+uint8_t get_address();
+uint8_t get_control();
+uint8_t get_flag();
 state get_curr_state();
-
-/**
- * @brief Get the curr role object
- * @return role 
- */
 role get_curr_role();
-
-/**
- * @brief Get the curr command object
- * @return command 
- */
 command get_curr_command();
-
-/**
- * @brief Get the prev response object
- * @return response 
- */
 response get_prev_response();
 
-
-// Setters
-/**
- * @brief Set the address object
- * @param s 
- */
-void set_address(unsigned char s);
-
-/**
- * @brief Set the control object
- * @param s 
- */
-void set_control(unsigned char s);
-
-/**
- * @brief Set the state object
- * @param s 
- */
+////////////////////////////////////////////////
+// SETTERS
+////////////////////////////////////////////////
+void set_address(uint8_t s);
+void set_control(uint8_t s);
 void set_state(state s);
-
-/**
- * @brief Set the role object
- * @param r 
- */
 void set_role(role r);
-
-/**
- * @brief Set the command object
- * @param c 
- */
 void set_command(command c);
 
-/**
- * @brief Set the response object
- * @param r 
- */
-void set_response(response r);
-
-
-// Others functions
-/**
- * @brief updates the current state of the state machine based on the received byte
- * @param byte - to be processed by the state machine
- */
-void update_state(unsigned char byte);
-
-/**
- * @brief resets the state machine to its initial state
- */
+////////////////////////////////////////////////
+// OTHERS
+////////////////////////////////////////////////
+void update_state(uint8_t byte);
 void reset_state();
 
 #endif // _STATE_
